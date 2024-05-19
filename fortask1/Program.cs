cb014261ad2642cc4cce81f7bdff6e0792dc0c7a
samirhasanli <samirhasanlih@gmail.com>
2024-05-19 04:00:34 +0400

    task elave olundu

---
+++ fortask1/Program.cs
@@ -0,0 +1,32 @@
+﻿//Task 1
+
+//int a = 1;
+//int i = 1;
+
+//for (a = 1; a <= 5; a++)
+//{
+//    Console.WriteLine("a=" + a);
+
+
+//    for (i = 1; i <= 3; i++)
+//    {
+//        Console.WriteLine("i=" + i);
+//    }
+//    Console.WriteLine();
+//}
+//Console.Read();
+
+//Task 2
+
+//for (int i =1; i <=10; i++)
+//{
+//    for (int j = 1; j <=10; j++)
+
+//    {
+//        Console.Write(j*i+"  ");
+
+//    }
+//    Console.WriteLine();
+    
+//}
+//Console.Read();
